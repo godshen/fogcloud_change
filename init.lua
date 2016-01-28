ece945b8e8a4e3ba7dc077983776d19c53a4158e
godshen <shenzh0713@gmail.com>
2016-01-28 20:56:08 +0800

    v0.7

--- init.lua
+++ init.lua
@@ -73,8 +73,8 @@ function _M.decode(payload)
     local head2 = string.sub(payload,2,2)
 
     if (head1== ';' and head2=='1') then 
-      x = tonumber(string.sub(payload,3,3),16) ;
-      y = tonumber(string.sub(payload,4,4),16) ;
+      x = tointeger(string.sub(payload,3,3),16) ;
+      y = tointeger(string.sub(payload,4,4),16) ;
       packet[ cmds[2] ] = 1 * 3 + 4 * 2;
       --packet[ cmds[3] ] = tonumber(string.sub(payload,5,5),16) * 16777216 +tonumber(string.sub(payload,6,6),16) * 65536 +tonumber(string.sub(payload,7,7),16) * 256 +tonumber(string.sub(payload,8,8),16)
       --packet[ cmds[4] ] = tonumber(string.sub(payload,9,9),16) 
